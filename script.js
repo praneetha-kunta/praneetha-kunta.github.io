270aef48cb3d523d32021069dcb65d5a81e9db72
praneetha-kunta <134955464+praneetha-kunta@users.noreply.github.com>
2023-06-08 23:21:44 +0530

    add23

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var data1={image : "http://4.bp.blogspot.com/_445R3_lKy7g/TDmiSyspRsI/AAAAAAAABoY/ZsyQSEMZrYQ/s1600/Dr-APJ-Abdul-Kalam-speech.jpg", name : "APJ abdul kalam" ,designation : "former president of india"};
-var data2={image : "https://assets.website-files.com/5f34328036a27c22711defb7/5f4d16eb0fa1605dfe34d9b9_nick-v-2.jpg", name : "nick vujicic" ,designation : "motivational speaker"};
+var data1={image:"http://4.bp.blogspot.com/_445R3_lKy7g/TDmiSyspRsI/AAAAAAAABoY/ZsyQSEMZrYQ/s1600/Dr-APJ-Abdul-Kalam-speech.jpg", name:"APJ abdul kalam" ,designation:"former president of india"};
+var data2={image:"https://assets.website-files.com/5f34328036a27c22711defb7/5f4d16eb0fa1605dfe34d9b9_nick-v-2.jpg", name:"nick vujicic" ,designation:"motivational speaker"};
 var isdata1=true;
 var displaydata;
 var changemypic = function(){
